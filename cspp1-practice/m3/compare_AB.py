5403e853d850297cefaa044b29c1526a7735856c
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-01 19:25:50 +0530

    comparision

---
+++ cspp1-practice/m3/compare_AB.py
@@ -0,0 +1,17 @@
+'''
+Author:Gnanika
+Date:1 august 2018
+
+'''
+# Testing of comparing operators and if condition
+varA=2
+varB="gnani"
+if varA is str or varB is str:
+    print("string involved")
+elif varA>varB:
+	print("bigger")
+elif varA=varB:
+    print("equal")
+else
+    print("smaller")
+

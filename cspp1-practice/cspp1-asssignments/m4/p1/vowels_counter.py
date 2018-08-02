e61be7450d4565508261fe5da8d112bab7721534
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-02 17:07:22 +0530

    " -> 0 of 4 passed. pylint: 10.00/10 "

--- cspp1-practice/cspp1-asssignments/m4/p1/vowels_counter.py
+++ cspp1-practice/cspp1-asssignments/m4/p1/vowels_counter.py
@@ -4,12 +4,13 @@ Date:2 August 2018
 '''
 
 def main():
-    S_T = input()
-    N_V = 0
-    for ch_ar in S_T:
-        if ch_ar == 'a' or ch_ar == 'e' or ch_ar == 'i' or ch_ar == 'o' or ch_ar == 'u':
-             N_V += 1
-    print("Number of vowels:"+" "+str(N_V))
+    ''' printing the number of vowels'''
+    s_t = input()
+    n_v = 0
+    for ch_ar in s_t:
+        if ch_ar in ('a', 'e', 'i', 'o', 'u'):
+            n_v += 1
+    print("Number of vowels:"+" "+str(n_v))
 
 if __name__ == "__main__":
-	main()
+    main()

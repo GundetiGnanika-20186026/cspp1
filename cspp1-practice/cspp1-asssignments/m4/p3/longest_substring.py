11ee8f9cdaf134c15f94a83eba6ecfdad2ddf2d0
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-02 20:40:52 +0530

    " -> 2 of 5 passed. pylint: 10.00/10 "

---
+++ cspp1-practice/cspp1-asssignments/m4/p3/longest_substring.py
@@ -0,0 +1,34 @@
+'''
+Auther:Gnanika
+Date:2 August 2018
+
+'''
+
+def main():
+    '''finding largest substring'''
+    max1 = 0
+    c_t = 0
+    s_t = input()
+    s_t1 = " "
+    s_t3 = " "
+    i = 0
+    #s_t3 = s_t1[0]
+    for i in range(0, len(s_t)-1):
+        if s_t[i] <= s_t[i + 1]:
+            s_t1 = s_t1 + s_t[i]
+            c_t = c_t + 1
+        else:
+            s_t1 = s_t1 + s_t[i]
+            c_t = c_t + 1
+            if max1 < c_t:
+                s_t3 = s_t1
+                max1 = c_t
+            s_t1 = ""
+            c_t = 0
+    s_t1 = s_t1 + s_t[i+1]
+    if max1 <= c_t:
+        s_t3 = s_t1
+    print(s_t3)
+
+if __name__ == "__main__":
+    main()

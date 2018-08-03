acb9301b7f139ee2310ca152d214c54c60bd97ad
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-03 18:02:30 +0530

    " -> 0 of 4 passed. pylint: 10.00/10 "

---
+++ cspp1-practice/m5/p1/perfect_cube.py
@@ -0,0 +1,20 @@
+'''
+Author:Gnanika
+Date: 3 august 2018
+
+'''
+
+def main():
+    ''' perfect cube '''
+    in_num = int(input('Enter an integer: '))
+    count = 0
+    while count**3 < in_num:
+        count = count + 1
+    if count ** 3 == in_num:
+        print(str(in_num) + " is  a perfect cube")
+    else:
+        print(str(in_num) + "is not a perfect cube")
+
+
+if __name__ == "__main__":
+    main()

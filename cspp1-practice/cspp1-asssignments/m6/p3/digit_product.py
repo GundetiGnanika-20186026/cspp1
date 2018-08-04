de901e9b6cd9df55cc1bb6e60e8f01a5f4c9e941
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-04 11:20:25 +0530

    " -> 4 of 7 passed. pylint: 10.00/10 "

--- cspp1-practice/cspp1-asssignments/m6/p3/digit_product.py
+++ cspp1-practice/cspp1-asssignments/m6/p3/digit_product.py
@@ -5,14 +5,14 @@ Date: 4 August 2018
 '''
 def main():
     '''
-    calculating the sum of integers in the given number
+    calculating the products of integers in the given number
     '''
     int_input = int(input())
-    su_m = 0
+    product = 1
     while int_input > 0:
         reminde_r = int_input % 10
-        su_m = su_m + reminde_r
+        product = product * reminde_r
         int_input = int_input // 10
-    print(su_m)
+    print(product)
 if __name__ == "__main__":
     main()

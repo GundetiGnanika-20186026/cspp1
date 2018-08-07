d16aaead6caafec868f267ca2858ad607898963b
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-07 20:31:54 +0530

    finding a character in string

---
+++ cspp1-practice/cspp1-asssignments/m8/Is In Exercise/is_in.py
@@ -0,0 +1,32 @@
+'''
+Author:Gnanika
+Date: 7 August 2018
+'''
+def isin(char, astr):
+    '''doing sorting of given string'''
+    sort_str = sorted(astr)
+    boolean = isin_2(0,len(sort_str),char, sort_str)
+    return boolean
+def isin_2(min_um, max_um, char, astr):
+    '''
+    char:a single character
+    astr:an alphabetical string
+    it returns true if char is present in astr otherwise false
+    '''
+    mid = (min_um + max_um)//2
+    if astr[mid] == char:
+        return "True"
+    elif mid in (min_um, max_um):
+        return "False"
+    else:
+        if astr[mid] > char:
+            return isin_2(min_um, mid, char, astr)
+        elif astr[mid] < char:
+            return isin_2(mid, max_um, char, astr)
+def main():
+    '''main function'''
+    data = input()
+    data = data.split()
+    print(isin((data[0][0]), data[1]))
+if __name__ == "__main__":
+    main()

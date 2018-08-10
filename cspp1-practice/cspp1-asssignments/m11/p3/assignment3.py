250132ec927a49770294f28d8637deb6c1517397
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-10 18:36:31 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- cspp1-practice/cspp1-asssignments/m11/p3/assignment3.py
+++ cspp1-practice/cspp1-asssignments/m11/p3/assignment3.py
@@ -1,47 +1,39 @@
-# Assignment-3
 '''
-At this point, we have written code to generate a random hand and display that hand to the user. We can also ask the user for a word (Python's input) and score the word (using your getWordScore). However, at this point we have not written any code to verify that a word given by a player obeys the rules of the game. A valid word is in the word list; and it is composed entirely of letters from the current hand. Implement the isValidWord function.
-
-Testing: Make sure the test_isValidWord tests pass. In addition, you will want to test your implementation by calling it multiple times on the same hand - what should the correct behavior be? Additionally, the empty string ('') is not a valid word - if you code this function correctly, you shouldn't need an additional check for this condition.
-
-Fill in the code for isValidWord in ps4a.py and be sure you've passed the appropriate tests in test_ps4a.py before pasting your function definition here.
+Author:Gnanika
+Date:10 August 2018
 '''
 
-def isValidWord(word, hand, wordlist):
+def isvalidword(word, hand, wordlist):
     """
     Returns True if word is in the wordList and is entirely
     composed of letters in the hand. Otherwise, returns False.
 
     Does not mutate hand or wordList.
-   
     word: string
     hand: dictionary (string -> int)
     wordList: list of lowercase strings
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    c = 0
+    count = 0
     leng_th = len(word)
     if word in wordlist:
         for i in word:
-           for j in hand:
-               if i == j:
-                   c = c + 1
-    if c == leng_th:
-        return "True"	
+            for j in hand:
+                if i == j:
+                    count = count + 1
+    if count == leng_th:
+        return "True"
     return "False"
 
 def main():
-	word=input()
-	n=int(input())
-	adict={}
-	for i in range(n):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	l2=input().split()
-	print(isValidWord(word,adict,l2))
-		
-
-
-if __name__== "__main__":
-	main()
+    '''main function'''
+    word = input()
+    num = int(input())
+    adict = {}
+    for _ in range(num):
+        data = input()
+        lis_t = data.split()
+        adict[lis_t[0]] = int(lis_t[1])
+    lis_t2 = input().split()
+    print(isvalidword(word, adict, lis_t2))
+if __name__ == "__main__":
+    main()

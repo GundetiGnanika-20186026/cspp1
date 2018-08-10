321849f63bbbcbe792167c9d6dde6b42e73f8cdc
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-10 16:33:53 +0530

    " -> 9 of 9 passed. pylint: 4.67/10 "

--- cspp1-practice/cspp1-asssignments/m11/p1/assignment1.py
+++ cspp1-practice/cspp1-asssignments/m11/p1/assignment1.py
@@ -23,15 +23,15 @@ def get_word_score(word, num):
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
     'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
     's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
-}
-sum = 0
-leng_th=len(word)
-for i in word:
-    sum = sum + SCRABBLE_LETTER_VALUES[i]
-if num == leng_th:
-    return (sum * leng_th)+50
-else:
-    return sum * leng_th
+    }
+    sum = 0
+    leng_th=len(word)
+    for i in word:
+        sum = sum + SCRABBLE_LETTER_VALUES[i]
+    if num == leng_th:
+        return (sum * leng_th)+50
+    else:
+        return sum * leng_th
 
 
 def main():

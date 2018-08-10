745f2a7c296dcdf1d9f6a94721ab0acf7e414feb
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-10 17:33:17 +0530

    " -> 6 of 6 passed. pylint: 10.00/10 "

--- cspp1-practice/cspp1-asssignments/m11/p2/assignment2.py
+++ cspp1-practice/cspp1-asssignments/m11/p2/assignment2.py
@@ -1,41 +1,35 @@
-#Exercise: Assignment-2
-#Implement the updateHand function. Make sure this function has no side effects: i.e., it must not mutate the hand passed in. Before pasting your function definition here, be sure you've passed the appropriate tests in test_ps4a.py.
-
-
-def updateHand(hand, word):
+'''
+Author:Gnanika
+Date:10 August 2018
+'''
+def updatehand(hand, word):
     """
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
-
+    many of that letter in it.
     Updates the hand: uses up the letters in the given word
     and returns the new hand, without those letters in it.
-
-    Has no side effects: does not modify hand.
-
+    Has no side effects: does not modify hand.s
     word: string
-    hand: dictionary (string -> int)    
+    hand: dictionary (string -> int)
     returns: dictionary (string -> int)
     """
     # TO DO ... <-- Remove this comment when you code this function
-    for i in hand:
-        for j in word:
-            if i == j:
-                hand[i]=hand[i]-1 
+    for i_letter in hand:
+        for j_letter in word:
+            if i_letter == j_letter:
+                hand[i_letter] = hand[i_letter]-1
     return hand
-
 def main():
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	data1=input()
-	print(updateHand(adict,data1))
-		
-
-
-if __name__== "__main__":
-	main()
+    '''main function'''
+    num = input()
+    adict = {}
+    for _ in range(int(num)):
+        data = input()
+        li_st = data.split()
+        adict[li_st[0]] = int(li_st[1])
+    data1 = input()
+    print(updatehand(adict, data1))
+if __name__ == "__main__":
+    main()

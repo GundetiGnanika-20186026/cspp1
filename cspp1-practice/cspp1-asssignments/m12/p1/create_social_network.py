9b4dcc50d7afcf9a4986374c15b423d05031924d
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-11 12:48:02 +0530

    " -> 0 of 3 passed. pylint: 9.05/10 "

--- cspp1-practice/cspp1-asssignments/m12/p1/create_social_network.py
+++ cspp1-practice/cspp1-asssignments/m12/p1/create_social_network.py
@@ -43,7 +43,7 @@ def create_social_network(data):
     for j in li_st2[1:len(li_st2+1):2]:
         #l2=j.split(":")
         #l2[1] = l2[1].split(",")
-        k =k +list(j)
+        k =k +j.list(j)
     print(k)
         #adict[j[0]] = j[1]
     return adict

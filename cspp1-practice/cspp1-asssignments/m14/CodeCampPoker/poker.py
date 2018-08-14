c9befcab9cf7b20005b96ca7d08351b62261975e
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-14 17:17:54 +0530

    " -> 3 of 3 passed. pylint: 9.79/10 "

--- cspp1-practice/cspp1-asssignments/m14/CodeCampPoker/poker.py
+++ cspp1-practice/cspp1-asssignments/m14/CodeCampPoker/poker.py
@@ -1,9 +1,7 @@
 '''
-    Write a program to evaluate poker hands and determine the winner
-    Read about poker hands here.
-    https://en.wikipedia.org/wiki/List_of_poker_hands
+    Author:Gnanika
+    Date:14 August 2018
 '''
-
 def is_straight(hand):
     '''
         How do we find out if the given hand is a straight?
@@ -15,19 +13,17 @@ def is_straight(hand):
         Write the code for it and return True if it is a straight else return False
     '''
     sequence = '23456789TJQKA'
-    my_dict = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
-    list_1 = [] 
+    my_dict = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7,
+                '8':8, '9':9, 'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+    list_1 = []
     for i in my_dict:
-    	for j in hand:
-    	    if i == j[0]:
-    	    	list_1.append(i[0])
+        for j in hand:
+            if i == j[0]:
+                list_1.append(i[0])
     for i in range(len(sequence)-4):
-    	if sequence[i:i+5] == ''.join(list_1):
-    		return True
+        if sequence[i:i+5] == ''.join(list_1):
+            return True
     return False
-
-    
-
 def is_flush(hand):
     '''
         How do we find out if the given hand is a flush?
@@ -36,26 +32,23 @@ def is_flush(hand):
         The second character is good enough to determine a flush
         Think of an algorithm: given the card suite how to check if it is a flush
         Write the code for it and return True if it is a flush else return False
-    .'''
-#############################################################################
+    '''
     count_dimd = 0
     count_spade = 0
     count_clubs = 0
     count_hearts = 0
     for i in hand:
-    	if i[1] == 'D':
-    	    count_dimd += 1
-    	if i[1] == 'S':
-    	    count_spade += 1
-    	if i[1] == 'C':
-    	    count_clubs += 1
-    	if i[1] == 'H':
-    	    count_hearts += 1
-    if count_dimd == 5 or count_spade == 5 or count_clubs == 5 or count_hearts == 5: 
+        if i[1] == 'D':
+            count_dimd += 1
+        if i[1] == 'S':
+            count_spade += 1
+        if i[1] == 'C':
+            count_clubs += 1
+        if i[1] == 'H':
+            count_hearts += 1
+    if count_dimd == 5 or count_spade == 5 or count_clubs == 5 or count_hearts == 5:
         return True
-    
     return False
-##############################################################################################
 def hand_rank(hand):
     '''
         You will code this function. The goal of the function is to
@@ -64,7 +57,6 @@ def hand_rank(hand):
         The first version should identify if the given hand is a straight
         or a flush or a straight flush.
     '''
-
     # By now you should have seen the way a card is represented.
     # If you haven't then go the main or poker function and print the hands
     # Each card is coded as a 2 character string. Example Kind of Hearts is KH
@@ -73,29 +65,23 @@ def hand_rank(hand):
     # What would be the logic to determine if a hand is a straight or flush?
     # Let's not think about the logic in the hand_rank function
     # Instead break it down into two sub functions is_straight and is_flush
-
     # check for straight, flush and straight flush
     # best hand of these 3 would be a straight flush with the return value 3
     # the second best would be a flush with the return value 2
     # third would be a straight with the return value 1
     # any other hand would be the fourth best with the return value 0
     # max in poker function uses these return values to select the best hand
-##########################################################################################3
     # best hand of these 3 would be a straight flush with the return value 3
     if is_flush(hand) and is_straight(hand):
-        return 3 
+        return 3
      # the second best would be a flush with the return value 2
     if is_flush(hand):
-    	return 2
+        return 2
     # third would be a straight with the return value 1
     if is_straight(hand):
-        return 1 
+        return 1
      # any other hand would be the fourth best with the return value 0
     return 0
-
-
-
-####################################################################################
 def poker(hands):
     '''
         This function is completed for you. Read it to learn the code.
@@ -106,8 +92,6 @@ def poker(hands):
 
         Output: Return the winning poker hand
     '''
-    
-
     # the line below may be new to you
     # max function is provided by python library
     # learn how it works, in particular the key argument, from the link
@@ -116,7 +100,6 @@ def poker(hands):
     # hand_rank takes a hand and returns its rank
     # max uses the rank returned by hand_rank and returns the best hand
     return max(hands, key=hand_rank)
-
 if __name__ == "__main__":
     # read the number of test cases
     COUNT = int(input())

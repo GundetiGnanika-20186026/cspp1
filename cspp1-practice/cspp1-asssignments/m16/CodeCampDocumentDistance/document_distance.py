561496748b563a6a35f34433317ae4ab04cfb3ea
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-17 16:54:29 +0530

    " -> 0 of 3 passed. pylint: 8.10/10 "

--- cspp1-practice/cspp1-asssignments/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-practice/cspp1-asssignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -51,6 +51,7 @@ def similarity(dict1, dict2):
             if i == j:
                 list_2.remove(j) 
     finaldict = wordfrequency(list_1,list_2)
+    print(sorted(finaldict.keys()))
     numerator = 0
     sum1 = 0
     sum2 = 0

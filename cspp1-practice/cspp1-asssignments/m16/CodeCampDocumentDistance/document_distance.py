2bb0fd3987137a9a6c65419f637ba8d7d35c2633
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-17 16:19:15 +0530

    " -> 0 of 3 passed. pylint: 7.26/10 "

--- cspp1-practice/cspp1-asssignments/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-practice/cspp1-asssignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -1,14 +1,65 @@
 '''
     Document Distance - A detailed description is given in the PDF
 '''
+import math
+def wordfrequency(list_1,list_2):
+    wordf1 = {}
+    wordf2 = {}
+    adict = {}
+    for i in list_1:
+        if i in wordf1:
+            wordf1[i] += 1
+        else:
+            wordf1[i] = 1
+    
+    for i in list_2:
+        if i in wordf2:
+            wordf2[i] += 1
+        else:
+            wordf2[i] = 1
+    for i in wordf1:
+        if i in wordf2:
+            adict[i] = [wordf1[i],wordf2[i]]
+        else:
+            adict[i] = [wordf1[i],0]
+    for i in wordf2:
+        if i not in adict:
+            adict[i] = [0,wordf2[i]]
+    return adict
 
 def similarity(dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
-    print(dict1)
-    pass
-
+    list_1 = []
+    list_2 = []
+    doc_1 = dict1.lower()
+    doc_2 = dict2.lower()
+    doc_11 = doc_1.split(" ")
+    doc_22 = doc_2.split(" ")
+    for i in doc_11:
+        list_1.append(i.strip().strip('.?,'))
+    for i in doc_22:
+        list_2.append(i.strip().strip('.?,'))
+    
+    for i in load_stopwords("stopwords.txt"):
+        for j in list_1:
+            if i == j:
+                list_1.remove(j) 
+    for i in load_stopwords("stopwords.txt"):
+        for j in list_2:
+            if i == j:
+                list_2.remove(j) 
+    finaldict = wordfrequency(list_1,list_2)
+    numerator = 0
+    sum1 = 0
+    sum2 = 0
+    for i in finaldict:
+        numerator = numerator + i[0]*i[1]
+        sum1 = sum1 + (i[0]*i[0]) 
+        sum2 = sum2 + (i[1]*i[1])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+    similarity = numerator/denominator
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
@@ -25,7 +76,8 @@ def main():
     '''
     input1 = str(input())
     input2 = str(input())
-
+    
+    
     print(similarity(input1, input2))
 
 if __name__ == '__main__':

47f7074e4225b160a2b286d8cd4e8fec94b8a946
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-18 11:28:06 +0530

    " -> 0 of 1 passed. pylint: 6.67/10 "

--- cspp1-practice/cspp1-asssignments/m18/p1/build_search_index.py
+++ cspp1-practice/cspp1-asssignments/m18/p1/build_search_index.py
@@ -65,7 +65,7 @@ def build_search_index(docs):
     i=0
     for j in docs:
         searchindex[j] = (i,)
-        searchindex.split()
+        j.split()
         i = i+1
     print(searchindex)
     '''

8ec6374718dae82c468acd79e30e707166a69c96
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-19 11:54:15 +0530

    " -> 0 of 1 passed. pylint: 5.00/10 "

--- cspp1-practice/cspp1-asssignments/m18/p2/search.py
+++ cspp1-practice/cspp1-asssignments/m18/p2/search.py
@@ -45,6 +45,7 @@ def search(search_index, query):
         if i in search_index:
                 
                 list2=list(search_index.get(i))
+                print(list2)
                 for j in range(len(list2)):
                     list1.add(list2[j][0][0])
     return list1

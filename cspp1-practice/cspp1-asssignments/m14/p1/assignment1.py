82cbaa167f13901b8d1bff4c6519aab4e63df62a
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-22 00:21:07 +0530

    " -> 0 of 5 passed. pylint: 7.83/10 "

---
+++ cspp1-practice/cspp1-asssignments/m14/p1/assignment1.py
@@ -0,0 +1,27 @@
+import string
+class Cipher:
+    def __init__(self, text):
+        self.text = text
+
+    def __len__(self):
+        count = 0
+        for i in self.text:
+            count += 1
+        return count
+
+    def shift(self, shift_number):
+        small_value = ""
+        large_value = ""
+        small_value = "-" + string.ascii_lowercase + string.ascii_lowercase
+        large_value = "-" + string.ascii_uppercase + string.ascii_uppercase
+        final_str = ""
+        a = len(self.text)
+        for i in range(0, a):
+            if self.text[i] in small_value:
+                final_str += small_value[small_value.index(self.text[i]) + shift_number]
+            elif self.text[i] in large_value:
+                final_str += large_value[large_value.index(self.text[i]) + shift_number]
+            else:
+                final_str += self.text[i]
+
+        return final_str

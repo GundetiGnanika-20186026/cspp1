d33b3fa001f2020559c01637914aeecf63bab471
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-23 20:20:36 +0530

    " -> 7 of 8 passed. pylint: 7.89/10 "

--- cspp1-practice/cspp1-asssignments/m20/CodeCampMatrixOperations/matrix/matrix_operations.py
+++ cspp1-practice/cspp1-asssignments/m20/CodeCampMatrixOperations/matrix/matrix_operations.py
@@ -2,40 +2,40 @@
 Author:Gnanika
 Date:23 August 2018
 '''
-def mult_matrix(m_1, m_2, r_1, c_1, r_2, c_2):
+def mult_matrix(m_1, m_2, r_1, c_1_1, r_2, c_1_2):
     '''
-        check if the matrix1 columns = matrix2 rows
-        mult the matrices and return the result matrix
+        c_1hec_1k if the matrix1 c_1olumns = matrix2 rows
+        mult the matric_1es and return the result matrix
         print an error message if the matrix shapes are not valid for mult
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
-    if  c_1 == r_2:
+    if  c_1_1 == r_2:
         mulresult = []
         for i in range(r_1):
             list1 = []
             for j in range(r_1):
                 sum_1 = 0
-                for k in range(c_1):
+                for k in range(c_1_1):
                     sum_1 = sum_1 + (m_1[i][k] * m_2[k][j])
                 list1.append(sum_1)
             mulresult.append(list1)
         return mulresult
     print("Error: Matrix shapes invalid for mult")
     return None        
-def add_matrix(m_1, m_2, r_1, c_1, r_2, c_2):
+def add_matrix(m_1, m_2, r_1, c_1_1, r_2, c_1_2):
     '''
-        check if the matrix shapes are similar
-        add the matrices and return the result matrix
+        c_1hec_1k if the matrix shapes are similar
+        add the matric_1es and return the result matrix
         print an error message if the matrix shapes are not valid for addition
         and return None
         error message should be "Error: Matrix shapes invalid for addition"
     '''
-    if r_1 == r_2 and c_1 == c_2:
+    if r_1 == r_2 and c_1_1 == c_1_2:
         addition = []
         for i in range(r_1):
             list1 = []
-            for j in range(c_1):
+            for j in range(c_1_1):
                 list1.append(m_1[i][j] + m_2[i][j])
             addition.append(list1)
         return addition
@@ -44,8 +44,8 @@ def add_matrix(m_1, m_2, r_1, c_1, r_2, c_2):
 def read_matrix():
     '''
         read the matrix dimensions from input
-        create a list of lists and read the numbers into it
-        in case there are not enough numbers given in the input
+        c_1reate a list of lists and read the numbers into it
+        in c_1ase there are not enough numbers given in the input
         print an error message and return None
         error message should be "Error: Invalid input for the matrix"
 
@@ -54,7 +54,7 @@ def read_matrix():
     dim_11 = dim_1.split(",")
     range1 = []
     list1 = []
-    c = 0
+    c_1 = 0
     for i in dim_11:
         i = int(i)
         range1.append(i)
@@ -69,23 +69,23 @@ def read_matrix():
         list1 += [rowlist]
     for i in list1:
         for j in i:
-            c = c + 1
-    if c == range1[0] * range1[1]:
+            c_1 = c_1 + 1
+    if c_1 == range1[0] * range1[1]:
         return list1,range1[0],range1[1]
     else:
         print("Error: Invalid input for the matrix")
         return [],range1[0],range1[1]
 def main():
-    '''main function'''
+    '''main func_1tion'''
     # read matrix 1
-    (matrix_1, row_1, column_1) = read_matrix()
+    (matrix_1, row_1, c_1olumn_1) = read_matrix()
     # read matrix 2
-    (matrix_2, row_2, column_2) = read_matrix()
+    (matrix_2, row_2, c_1olumn_2) = read_matrix()
     if not matrix_2 == []:
         # add matrix 1 and matrix 2
-        print(add_matrix(matrix_1, matrix_2, row_1, column_1, row_2, column_2))
+        print(add_matrix(matrix_1, matrix_2, row_1, c_1olumn_1, row_2, c_1olumn_2))
         # multiply matrix 1 and matrix 2
-        print(mult_matrix(matrix_1, matrix_2, row_1, column_1, row_2, column_2))
+        print(mult_matrix(matrix_1, matrix_2, row_1, c_1olumn_1, row_2, c_1olumn_2))
 
 if __name__ == '__main__':
     main()

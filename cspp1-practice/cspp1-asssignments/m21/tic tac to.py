fe0430ed6590ece3da95885e70d98c331934c4ef
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-24 15:00:47 +0530

    " -> 0 of 14 passed. pylint: 6.00/10 "

--- cspp1-practice/cspp1-asssignments/m21/tic tac to.py
+++ cspp1-practice/cspp1-asssignments/m21/tic tac to.py
@@ -1,5 +1,18 @@
-
-
+def horizontal(list1):
+    for i in list1:
+        if i[0] == i[1] and i[1] == i[2]:
+            return i[0]
+        return False
+def vertical(list1):
+    for i in list1:
+        if list1[0][i] == list1[1][i] and list1[1][i] == list1[2][i]:
+             return list1[0][i]
+        return False
+def diagonal(list1):
+    c = list1[1][1] 
+    if (list1[0][0] == c and c == list1[2][2]) or (list1[0][2] == c and list1[2][0]):
+        return list1[0][0]
+    return False
 
 def read_input():
     list1=[]
@@ -17,9 +30,9 @@ def read_input():
                 o_count += 1
             if j == '.':
                 dot_count += 1
-            if j not in('x','o','.'):
+            if j not in('x', 'o' ,'.'):
                 return "invalid input"
-    if x_count > o_count + 1 or o_count > x_count + 1 :
+    if x_count > o_count + 1 or o_count > x_count + 1:
         return "invalid game"
     return list1
 
@@ -30,7 +43,13 @@ def read_input():
 
 def main():
     #read the input
-    print(read_input())
+    list1 = read_input()
+    horizon = horizontal(list1)
+    verti = vertical(list1)
+    diag = diagonal(list1)
+    if  horizon == False and verti == False and diag == False:
+        print("draw")
+    
     
 
 if __name__ == '__main__':

07cfa1ac56367cfe3b023ad479419c5f148c7072
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-24 14:13:35 +0530

    " -> 0 of 14 passed. pylint: 0/0 "

--- cspp1-practice/cspp1-asssignments/m21/tic tac to.py
+++ cspp1-practice/cspp1-asssignments/m21/tic tac to.py
@@ -4,20 +4,20 @@
 def read_input():
     list1=[]
     for i in range(3):
-        input1 = input().split()
-        list1.append(input1)
+        input1 = input()dotsplit()
+        list1dotappend(input1)
     x_count = 0
     o_count = 0
-    ._count = 0
+    dot_count = 0
     for i in list1:
         for j in i:
             if j == 'x':
                 x_count += 1
             if j == 'o':
                 o_count += 1
-            if j == '.':
-                ._count += 1
-            if j not in('x','o','.'):
+            if j == 'dot':
+                dot_count += 1
+            if j not in('x','o','dot'):
                 return "invalid input"
     if x_count > o_count + 1 or o_count > x_count + 1 :
         return "invalid game"

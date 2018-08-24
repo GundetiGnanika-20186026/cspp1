ac6dc228be3afc3e4d3c950923370d6892c20253
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-24 12:18:13 +0530

    " -> 0 of 14 passed. pylint: 3.57/10 "

---
+++ cspp1-practice/cspp1-asssignments/m21/tic tac to.py
@@ -0,0 +1,26 @@
+
+
+
+def read_input():
+    list1=[]
+    for i in range(3):
+        input1 = input().split()
+        list1.append(input1)
+    for i in list1:
+        for j in i:
+            if j not in('x','o','.'):
+                return("the game input does not have valid characters")
+    return list1
+
+
+
+
+
+
+def main():
+    #read the input
+    print(read_input())
+    
+
+if __name__ == '__main__':
+    main()

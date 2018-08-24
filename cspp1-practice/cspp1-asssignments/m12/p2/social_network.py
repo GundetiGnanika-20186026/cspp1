bef6d640b18c4e6a84ed91c0fe1c31db279daa0b
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-24 09:50:05 +0530

    " -> 2 of 2 passed. pylint: 9.39/10 "

--- cspp1-practice/cspp1-asssignments/m12/p2/social_network.py
+++ cspp1-practice/cspp1-asssignments/m12/p2/social_network.py
@@ -50,7 +50,7 @@ def main():
     '''
         handling testcase input and printing output
     '''
-    network = input()
+    network =eval(input())
     lines = int(input())
     for i in range(lines):
         i += 1

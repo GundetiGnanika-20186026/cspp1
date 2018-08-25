f5a26091a523274b9a3d7351d0a5914c53e1583e
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-25 11:06:58 +0530

    " -> 1 of 3 passed. pylint: 4.55/10 "

--- cspp1-practice/cspp1-asssignments/m22/assignment2/assignment2/clean_input.py
+++ cspp1-practice/cspp1-asssignments/m22/assignment2/assignment2/clean_input.py
@@ -8,7 +8,7 @@ def clean_string(string):
 
     str2 = ''
     for i in string:
-        if  i in (0,1,2,3,4,5,6,7,8,9) or('a' <= i >= 'z') or ('A' <= i >= 'Z'): 
+        if  i in ((0, 1, 2, 3, 4, 5, 6, 7, 8, 9) or ('a' <= i >= 'z') or ('A' <= i >= 'Z')): 
             str2 += i
     
     return str2

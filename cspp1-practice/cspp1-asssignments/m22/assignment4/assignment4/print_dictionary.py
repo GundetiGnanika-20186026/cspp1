fdff513ebd973e7adbfa08540d3a2b4c9fb7b9e4
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-25 12:55:32 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1-practice/cspp1-asssignments/m22/assignment4/assignment4/print_dictionary.py
+++ cspp1-practice/cspp1-asssignments/m22/assignment4/assignment4/print_dictionary.py
@@ -6,9 +6,9 @@ def print_dictionary(dictionary):
     '''printing the dictionary'''
     sort1 = sorted(dictionary)
     for i in sort1:
-    	    dictionary[i] = ''
-    	for j in range(dictionary[i])
-    	    dictionary[i]+='#'
+            dictionary[i] = ''
+        for j in range(dictionary[i]):
+            dictionary[i]+='#'
     print(sort1)    
     #for i in sort1:
         #print(i+" "+"-"+" "+str(dictionary[i]))

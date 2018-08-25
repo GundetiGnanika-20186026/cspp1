ff43a0d526ce65262309e1b113ee7e295eadb61b
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-25 11:20:09 +0530

    " -> 0 of 3 passed. pylint: -4.55/10 "

--- cspp1-practice/cspp1-asssignments/m22/assignment2/assignment2/clean_input.py
+++ cspp1-practice/cspp1-asssignments/m22/assignment2/assignment2/clean_input.py
@@ -2,19 +2,20 @@
 Write a function to clean up a given string by removing the special characters and retain 
 alphabets in both upper and lower case and numbers.
 '''
-
 def clean_string(string):
-    str2 = ''
-    for i in string:
-        if  i in ('0','1','2','3','4','5','6','7','8','9') or ('a' <= i >= 'z') or ('A' <= i >= 'Z'): 
-            str2 += i
-    if len(str2) == 0:
-        return
-    return str2
+    # str2 = ''
+    # for i in string:
+    #     if  i in ('0','1','2','3','4','5','6','7','8','9') or ('a' <= i >= 'z') or ('A' <= i >= 'Z'): 
+    #         str2 += i
+    # return str2
 
+    str2 = string[:]
+    for i in str2:
+        if  i not in ('0','1','2','3','4','5','6','7','8','9') or ('a' <= i >= 'z') or ('A' <= i >= 'Z'): 
+            string.remove(i)
+    return string
 def main():
     string = input()
     print(clean_string(string))
-
 if __name__ == '__main__':
     main()

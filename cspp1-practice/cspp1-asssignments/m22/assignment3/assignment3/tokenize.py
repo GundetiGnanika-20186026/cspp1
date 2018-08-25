4f6cf49fa20cd9e27d2cbb4eaaea227956a9a63f
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-25 11:35:20 +0530

    " -> 2 of 3 passed. pylint: 9.09/10 "

---
+++ cspp1-practice/cspp1-asssignments/m22/assignment3/assignment3/tokenize.py
@@ -0,0 +1,19 @@
+'''
+Write a function to tokenize a given string and return a dictionary with the frequency of
+each word
+'''
+import collections
+def tokenize(string):
+    adict = collections.Counter(string)
+    return adict        
+def main():
+	str1 ='' 
+	input1 = int(input())
+	for i in range(input1):
+		input2 = input()
+        str1 = input2
+        dict1 = tokenize(str1)
+    print(dict1)
+
+if __name__ == '__main__':
+    main()

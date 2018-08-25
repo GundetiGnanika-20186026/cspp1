bdfc5fb805270dbfda9976fca84d9e29c20da0a0
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-25 13:25:42 +0530

    " -> 3 of 4 passed. pylint: 9.20/10 "

--- cspp1-practice/cspp1-asssignments/m22/assignment3/assignment3/tokenize.py
+++ cspp1-practice/cspp1-asssignments/m22/assignment3/assignment3/tokenize.py
@@ -3,6 +3,7 @@ Author:Gnanika
 Date: 25 August
 '''
 def tokenize(string):
+    '''tokenising of given string'''
     adict = {}
     list1 = string.split()
     for i in list1:
@@ -12,6 +13,7 @@ def tokenize(string):
             adict[i] = 1
     return adict    
 def main():
+    '''main function'''
     str2 = ''
     str3 = ''
     input1 = int(input())
@@ -26,8 +28,6 @@ def main():
             for j in i:
                 if  ('a' <= j >= 'z') or ('A' <= j >= 'Z') or ' ':
                     str3 += j
-        
         print(tokenize(str3))
-
 if __name__ == '__main__':
     main()

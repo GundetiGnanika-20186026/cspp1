bce03a456cb6899d36f294a1ffa5dc84d861cd3e
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-25 10:49:30 +0530

    " -> 1 of 3 passed. pylint: -2.73/10 "

--- cspp1-practice/cspp1-asssignments/m22/assignment2/assignment2/clean_input.py
+++ cspp1-practice/cspp1-asssignments/m22/assignment2/assignment2/clean_input.py
@@ -6,8 +6,8 @@ alphabets in both upper and lower case and numbers.
 def clean_string(string):
     str2 = ''
     for i in string:
-    	if ('a'<= i >= 'z') or ('A' <= i >= 'Z') or ('0' <= i >= '9'):
-    		str2 += i
+        if ('a'<= i >= 'z') or ('A' <= i >= 'Z') or i in (0,1,2,3,4,5,6,7,8,9):
+            str2 += i
     
     return str2
 
